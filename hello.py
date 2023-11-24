580be86aeaca92ea6073c7bfae865ed5bdbd8e86
Azamat-tilenbaev <takebrand.ofis@gmail.com>
2023-11-24 14:39:31 +0600

    mac changer

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
-print ("Hello World!")
-print ("Hello World 1")
-
+import subprocess
+subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:66", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
+subprocess.call("ifconfig", shell=True)
